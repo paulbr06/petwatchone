68c7e192339ba3448d6c5dde9769a269cea3a087
paul <email@paulbr.com>
2022-10-08 20:23:25 +0000

    updated text

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ const App = ({ signOut }) => {
 
   return (
     <View className="App">
-      <Heading level={1}>My Notes App</Heading>
+      <Heading level={1}>PetWatch.one</Heading>
       <View as="form" margin="3rem 0" onSubmit={createNote}>
         <Flex direction="row" justifyContent="center">
           <TextField
